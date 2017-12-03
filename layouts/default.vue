4d5d6995327dff1953fd0f38ea88bf044254a8d3
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2017-12-03 13:54:56 -0700

    Build global nav with layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    <header>
+      <nav class="flex w-75 justify-around f4 code pa1">
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/new">New</nuxt-link>
+        <nuxt-link to="/ask">Ask</nuxt-link>
+      </nav>
+    </header>
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
